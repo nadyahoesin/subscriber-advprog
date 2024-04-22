8fb4d1c297ef0f96dace4c6f3c0c69bf3091b904
Nadya Hoesin <nadya.hoesin@ui.ac.id>
2024-04-22 19:20:46 +0700

    Simulation slow subscriber

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
     fn handle(&self, message: Box<UserCreatedEventMessage> ) -> Result<(), HandleError> {
         let ten_millis = time::Duration::from_millis(1000); 
         let now = time::Instant::now();
-        // thread::sleep(ten_millis);
+        thread::sleep(ten_millis);
         println!("In Nadya's Computer [2106651673]. Message
         received: {:?}", message);
                 Ok(())
@@ -21,7 +21,7 @@ impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
 }
 
 fn main() {
-    let listener = CrosstownBus::new_queue_listener("amqp://guest:guest@localhost:567 2".to_owned()).unwrap();
+    let listener = CrosstownBus::new_queue_listener("amqp://guest:guest@localhost:5672".to_owned()).unwrap();
     _ = listener.listen("user_created".to_owned(), 
     UserCreatedHandler{}, crosstown_bus::QueueProperties {
          auto_delete: false, durable: false, use_dead_letter: true });
